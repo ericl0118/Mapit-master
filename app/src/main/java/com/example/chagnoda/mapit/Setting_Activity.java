1c8a435d7c25a772da9e5a8307f71c231da7ca88
ericl0118 <ericl0118@hotmail.com>
2016-03-21 17:34:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chagnoda/mapit/Setting_Activity.java
@@ -0,0 +1,7 @@
+package com.example.chagnoda.mapit;
+
+/**
+ * Created by chengli on 2016-03-21.
+ */
+public class Setting_Activity {
+}
---
+++ app/src/main/java/com/example/chagnoda/mapit/activity_main.java
@@ -0,0 +1,20 @@
+package com.example.chagnoda.mapit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by chagnoda on 16-03-21.
+ */
+public class activity_main extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
