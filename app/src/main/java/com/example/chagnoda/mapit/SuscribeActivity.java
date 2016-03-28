3a1098c5c0a3eeddd632489ac906619fa103c341
DavidChagnonMorin <dcmmm44@gmail.com>
2016-03-28 00:08:28 -0400

    FireBase fonctionnel Maintenant possible de créé un profil et de l'enregistrer sur FireBase

--- app/src/main/java/com/example/chagnoda/mapit/SuscribeActivity.java
+++ app/src/main/java/com/example/chagnoda/mapit/SuscribeActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+
 import org.w3c.dom.Text;
 
 /**
@@ -19,6 +21,7 @@ public class SuscribeActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_suscribe);
         suscribe = (Button)findViewById(R.id.suscribebutton);
         suscribe.setOnClickListener(this);
@@ -38,6 +41,8 @@ public class SuscribeActivity extends AppCompatActivity implements View.OnClickL
 
         // note: a implementer, condition d'acceptation d'un nouvel acompte
         Profile new_user = new Profile(username,email,password);
+        Firebase ref = new Firebase("https://sizzling-inferno-6141.firebaseio.com/Mapit/Profiles");
+        ref.child(email).setValue(new_user);
         // a implementer, ajouter le profil a firebase
         startActivity(new Intent("com.example.chagnoda.mapit.MainActivity"));
 
