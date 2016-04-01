9cf07c9751d09ec52a93008653058777e53247e4
DavidChagnonMorin <dcmmm44@gmail.com>
2016-04-01 16:31:11 -0400

    suscribe et debut gps location

--- app/src/main/java/com/example/chagnoda/mapit/MainActivity.java
+++ app/src/main/java/com/example/chagnoda/mapit/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.chagnoda.mapit;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,9 +19,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
+    View mapit_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mapit_button = findViewById(R.id.mapitbutton);
+        mapit_button.setOnClickListener(this);
+        Firebase ref = new Firebase("https://sizzling-inferno-6141.firebaseio.com/Mapit");
+        AuthData authData = ref.getAuth();
+        Toast.makeText(getApplicationContext(), authData.getUid().toString(), Toast.LENGTH_SHORT).show();
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -33,6 +53,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -87,4 +109,23 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onClick(View v) {
+        // Il fat faire la permission pour acceder au a la localisation
+        int MY_PERMISSION_ACCESS_COURSE_LOCATION = 1000;
+        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        }
+        LocationManager locationManager=    (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        MyLocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,locationListener);
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/chagnoda/mapit/NotConnectedActivity.java
+++ app/src/main/java/com/example/chagnoda/mapit/NotConnectedActivity.java
@@ -1,11 +1,14 @@
 package com.example.chagnoda.mapit;
 
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 
 /**
  * Created by David on 27/03/2016.
